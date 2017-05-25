c71e3eeabfd5b0f6e420553fe481e1e139286eb0
jtumms <john.tumminelli@tummsmedia.com>
2017-05-25 18:26:35 -0400

    Initial Commit

---
+++ LuhnAlgorithn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LuhnAlgorithn
+//
+//  Created by John Tumminelli on 5/25/17.
+//  Copyright © 2017 John Tumminelli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
